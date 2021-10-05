8665df8b75cd5d21f943a23b80afad8a51ac298d
Javier Del Rio <jav.dl.ponce@gmail.com>
2021-10-05 19:22:21 -0300

    Contacto Proveedor

--- OrderFood/urls.py
+++ OrderFood/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('logout', logout, name="logout"),
     path('generar-cuenta/',generar_cuenta_get, name="generar-cuenta"),
     path('generar-cuenta/encargado-cocina', auth_middleware(generar_cuenta_enc_cocina), name="encargado-cocina"),
-    path('generar-cuenta/encargado-convenio', auth_middleware(generar_cuenta_enc_convenio), name="encargado-convenio")
-
+    path('generar-cuenta/encargado-convenio', auth_middleware(generar_cuenta_enc_convenio), name="encargado-convenio"),
+    path('proveedor', proveedor, name="proveedor"),
 ]
