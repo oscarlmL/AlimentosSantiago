c3dabcf1bf7c2d57af3632733baf0486a9e418b4
Javier Del Rio <jav.dl.ponce@gmail.com>
2021-10-05 20:43:50 -0300

    formato proveedor

--- OrderFood/forms.py
+++ OrderFood/forms.py
@@ -6,4 +6,9 @@ class ProveedorForm(forms.ModelForm):
 
     class Meta: 
         model = Proveedor
-        fields = '__all__'
+        fields = ['rol_local', 'nom_proveedor','celular','descripcion']
+
+    rol_local = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    nom_proveedor = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    celular = forms.CharField(widget=forms.NumberInput(attrs={'class': 'form-control'}))
+    descripcion = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
