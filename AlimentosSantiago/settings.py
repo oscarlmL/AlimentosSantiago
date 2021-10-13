285f2f32f954031d68b1788ed72761173dab87e8
SimonAriel <77398399+SimonAriel@users.noreply.github.com>
2021-10-13 14:47:43 -0300

    as

--- AlimentosSantiago/settings.py
+++ AlimentosSantiago/settings.py
@@ -126,6 +126,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 import os
-MEDIA_URL = '/imagenes/menú/'
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR), "media"
 
