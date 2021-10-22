bf58b2bdd88a0186e028c2f71f8dd3c6eddd944f
cecsanhueza <64716180+cecsanhueza@users.noreply.github.com>
2021-10-22 15:49:32 -0300

    Revert "Revert "Update admin.py""
    This reverts commit d6a899c39cfc9a897dad2078a99dd8cd23bc8b38.

--- OrderFood/admin.py
+++ OrderFood/admin.py
@@ -3,3 +3,12 @@ from .models import *
 
 # Register your models here.
 
+class AdminNCconvenio(admin.ModelAdmin):
+    list_display = ['nom_enc_conv','rut_enc_conv']
+
+
+class AdminNCempresa(admin.ModelAdmin):
+    list_display = ['rut_emp','nom_emp', 'nom_gerente', 'cant_trabajadores','enc_convenio_id_enc_conv']
+
+admin.site.register(Empresa,AdminNCempresa)
+admin.site.register(EncConvenio,AdminNCconvenio)
